5e5843739eae6cc7630263d3e2932f8430499bfc
Anderson Lopes <anderson@lopessystem.com>
2023-02-10 22:07:07 +0100

    Includes docker/database configs

--- src/main/java/com/lopessystem/studentmanagment/StudentManagementApplication.java
+++ src/main/java/com/lopessystem/studentmanagment/StudentManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagmentApplication {
+public class StudentManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentManagmentApplication.class, args);
+        SpringApplication.run(StudentManagementApplication.class, args);
     }
 
 }
