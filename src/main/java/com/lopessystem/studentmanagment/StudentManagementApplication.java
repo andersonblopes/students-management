fae7619a5198c3a67fe01871e0be8ca7747ae260
Anderson Lopes <anderson@lopessystem.com>
2023-02-11 01:51:36 +0100

    Improves view

--- src/main/java/com/lopessystem/studentmanagment/StudentManagementApplication.java
+++ src/main/java/com/lopessystem/studentmanagment/StudentManagementApplication.java
@@ -1,13 +1,23 @@
 package com.lopessystem.studentmanagment;
 
+import com.lopessystem.studentmanagment.model.Student;
+import com.lopessystem.studentmanagment.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.OffsetDateTime;
+import java.util.Arrays;
 
 /**
  * The type Student management application.
  */
+@RequiredArgsConstructor
 @SpringBootApplication
-public class StudentManagementApplication {
+public class StudentManagementApplication implements CommandLineRunner {
+
+    private final StudentRepository studentRepository;
 
     /**
      * Main.
@@ -18,4 +28,14 @@ public class StudentManagementApplication {
         SpringApplication.run(StudentManagementApplication.class, args);
     }
 
+    @Override
+    public void run(final String... args) throws Exception {
+
+        final Student anderson = new Student(1L, "Anderson", "Lopes", "anderson@email.com", OffsetDateTime.now(), Boolean.TRUE);
+        final Student elaine = new Student(2L, "Elaine", "Lopes", "elaine@email.com", OffsetDateTime.now(), Boolean.TRUE);
+        final Student helena = new Student(3L, "Halena", "Lopes", "helena@email.com", OffsetDateTime.now(), Boolean.TRUE);
+        
+        studentRepository.saveAll(Arrays.asList(anderson, elaine, helena));
+
+    }
 }
